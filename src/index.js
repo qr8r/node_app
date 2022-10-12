d45e880b12b1a3ccebc477bb2eeb1d2aee4a0239
Josh White <josh.white@shopify.com>
2022-10-12 19:25:44 -0400

    sample title
    added a grid, server doin server things

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import cors from 'express'
-import express from 'express'
-import sqlite from 'sqlite3'
-import ejs from 'ejs'
+import cors from 'express';
+import express from 'express';
+import sqlite from 'sqlite3';
+import ejs from 'ejs';
+import path from 'path';
+
 
 const db = new sqlite.Database('../db.sqlite')
 
@@ -9,6 +11,7 @@ const app = express()
 const port = 4004
 
 app.set('view engine', 'ejs')
+app.use('/css',express.static('src/public/css'));
 
 app.get('/', function(request, response) {
   response.render('index')
