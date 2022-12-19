a9becf9e07565cf15a2b52a57f2bc436125b32eb
Brendan Deere <6353433+qr8r@users.noreply.github.com>
2022-12-19 10:32:15 -0500

    Update foobar.js

--- src/foobar.js
+++ src/foobar.js
@@ -1,15 +1,1 @@
-
-var my_favorite_color = "#112C85";
-
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-var my_favorite_color = "#112C85";
-
-
-var a_new_thing = "sdf";
+var myFavoriteColor = "#112C85";
